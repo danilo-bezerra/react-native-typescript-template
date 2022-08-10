7792e23e573a237b56473a8a7e3eb14f6332b5aa
Danilo Bezerra <86009742+danilo-bezerra@users.noreply.github.com>
2022-08-10 18:43:10 -0300

    add native base

--- App.tsx
+++ App.tsx
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  NativeBaseProvider,
+  Box,
+  VStack,
+  StatusBar,
+  Input,
+  Heading,
+} from "native-base";
+import { THEME } from "./src/styles/theme";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar />
+      <VStack flex={1} p={4} bg="gray.600">
+        <Heading textAlign="center" color="gray.100" py={10}>
+          Hello world
+        </Heading>
+      </VStack>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
